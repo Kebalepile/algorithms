8fac73189f7b19f82f8c8ac18cf8de58566efd1b
Kebalepile <44870957+Kebalepile@users.noreply.github.com>
2020-12-04 15:22:24 +0200

    Update queue.js

--- spareParts/queue.js
+++ spareParts/queue.js
@@ -5,71 +5,38 @@
 // Queue follows First-In-First-Out methodology, i.e.,
 // the data item stored first will be accessed first.
 
-// queue with an array.
 class Queue {
-	#items = [];
-	#length = 0;
-	#maxSize = 4;
-	/**
-	 *
-	 * @param {Element} elem
-	 * @description adds element to the back of the queue.
-	 */
-	enqueue(elem) {
-		if (!this.isFull()) {
-			this.#items.push(elem);
-			this.#length += 1;
-		}
+	#q = [];
+	enqueue(data) {
+		this.#q.push(data);
 	}
-	/**
-	 * @description removes element from front of the queue.
-	 */
+
 	dequeue() {
-		if (!this.isEmpty()) {
-			this.#items.shift();
-			this.#length -= 1;
-		}
+		return this.#q.shift();
 	}
-	/**
-	 * @description gets element from front of the queue
-	 * without removing it from the queue.
-	 * @returns {Element} element.
-	 */
+
 	peek() {
-		if (!this.isEmpty()) {
-			return this.#items[0];
-		}
-		return null;
+		return this.#q[0];
 	}
-	/**
-	 * @description checks if queue if full.
-	 * @returns {Boolean} boolean
-	 */
-	isFull() {
-		return this.#length === this.#maxSize ? true : false;
-	}
-	/**
-	 * @description checks if queue if empty.
-	 * @returns {Boolean} boolean
-	 */
+
 	isEmpty() {
-		return this.#length === 0 ? true : false;
+		return this.#q.length > 0 ? false : true;
 	}
 }
 
 const q = new Queue();
+
+q.enqueue('data');
+q.enqueue(2);
+q.enqueue(17 + 25);
+q.enqueue('last air bander.');
 console.log(q.isEmpty());
-q.enqueue('Rage doll.');
-q.enqueue('Bob the builder');
-q.enqueue('Teletubbies');
-q.enqueue('Dragon Ball Super');
-q.enqueue('Teen Titens');
-console.log(q.peek());
-q.dequeue();
-console.log(q.peek());
-q.dequeue();
-console.log(q.peek());
-q.dequeue();
-console.log(q.peek());
-q.dequeue();
-console.log(q.isFull());
+console.log(q.peek(), ' <----- at first position of the queue.\n');
+console.log(q.dequeue(), ' <--- removed from queue.\n');
+console.log(q.peek(), ' <----- at first position of the queue.\n');
+console.log(q.dequeue(), ' <--- removed from queue.\n');
+console.log(q.peek(), ' <----- at first position of the queue.\n');
+console.log(q.dequeue(), ' <--- removed from queue.\n');
+console.log(q.dequeue(), ' <--- removed from queue.\n');
+console.log(q.dequeue(), ' <--- removed from queue.\n');
+console.log(q.isEmpty());
