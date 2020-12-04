91cb1297b3e16f99a402f7dee8a4b62a515b707c
Kebalepile <44870957+Kebalepile@users.noreply.github.com>
2020-12-04 14:38:07 +0200

    Update singlyLinkedList.js

--- spareParts/singlyLinkedList.js
+++ spareParts/singlyLinkedList.js
@@ -1,78 +1,3 @@
-// Node constructor.
-function Node(data) {
-	this.data = data;
-	this.next = null;
-}
-
-// Linked list constructor.
-function LinkedList() {
-	this.head = null;
-	this.length = 0;
-}
-
-LinkedList.prototype.AddNode = function (newNode) {
-	if (this.head === null) {
-		this.head = newNode;
-	} else {
-		let curNode = this.head;
-		let prevNode = this.head;
-
-		while (curNode !== null) {
-			prevNode = curNode;
-			curNode = curNode.next;
-		}
-		prevNode.next = newNode;
-	}
-	this.length += 1;
-};
-
-LinkedList.prototype.Insert = function (newNode, index) {
-	if (index <= 0 || index > this.length) {
-		console.log(`specifed index: ${index}, is out of range.`);
-		return;
-	} else if (this.head !== null) {
-		insert.bind(this)();
-	} else {
-		console.log('Insertion not allowed on an empty list.');
-	}
-	function insert() {
-	
-		let curNode = this.head;
-		let prevNode = this.head;
-		while(index !== 0) {
-			prevNode = curNode;
-			curNode = curNode.next;
-			index -= 1;
-		}
-		newNode.next = curNode;
-		prevNode.next = newNode;
-		this.length += 1;
-
-	}
-};
-
-LinkedList.prototype.DisplayAllNodes = function () {
-	let node = this.head;
-	while (node !== null) {
-		console.log('\n -------------------start---------------\n', node, '\n ---------end--------- \n');
-		node = node.next;
-	}
-};
-LinkedList.prototype.Length = function () {
-	return this.length;
-};
-
-const lList = new LinkedList();
-lList.AddNode(new Node('10111'));
-lList.Insert(new Node('Chucky cheese'), 1);
-lList.AddNode(new Node('name:keba, age:23'));
-
-lList.AddNode(new Node(`meaning of life is 42.`));
-
-lList.Insert(new Node('Lebogang'), 4);
-lList.DisplayAllNodes();
-console.log(lList.Length());
-// with ES5 class syntax
 class Node {
 	constructor(data) {
 		this.data = data;
@@ -81,70 +6,103 @@ class Node {
 }
 
 class LinkList {
-	constructor() {
-		this.head = null;
-		this.length = 0;
-	}
-
-	GetLength() {
-		return this.length;
-	}
-	DisplayNodes() {
-		let curNode = this.head;
-		while (curNode !== null) {
-			console.log('-------------start----------\n', curNode, '\n---------------end-----------------');
-			curNode = curNode.next;
-		}
-	}
-	AddNode(node) {
-		if (this.head == null) {
-			this.head = node;
-		} else {
-			let curNode = this.head,
-				prevNode;
-
-			while (curNode !== null) {
-				prevNode = curNode;
-				curNode = curNode.next;
-			}
-
-			prevNode.next = node;
-		}
-
-		this.length += 1;
-	}
-	InsertNode(node, index) {
-		if (index < 0 || index > this.GetLength() || this.GetLength() === 0) {
-			console.log(`index => ${index}, is out of range.`);
+	#head = null;
+	#length = 0;
+	insert(data) {
+		let node = this.#head;
+		if (!node) {
+			this.#head = new Node(data);
+			this.#length += 1;
 			return;
 		}
-		let curNode = this.head,
-			prevNode,
-			count = 0;
-		while (index !== count) {
+
+		while (node.next) {
+			node = node.next;
+		}
+
+		node.next = new Node(data);
+		this.#length += 1;
+	}
+	next() {
+		let node = this.#head;
+
+		const value = () => {
+				let val = node?.data || null;
+				node = node?.next || null;
+				return val;
+			},
+			done = () => {
+				return node?.data ? false : true;
+			};
+		return {
+			value,
+			done,
+		};
+	}
+	del(data) {
+		let curNode = this.#head,
+			prevNode;
+		while (curNode) {
+			if (curNode.data === data) {
+				curNode = curNode.next;
+				break;
+			}
 			prevNode = curNode;
 			curNode = curNode.next;
-			count += 1;
 		}
 
-		if (prevNode === undefined) {
-			node.next = curNode;
-			this.head = node;
+		if (prevNode) {
+			prevNode.next = curNode;
+			this.#head = prevNode;
 		} else {
-			node.next = curNode;
-			prevNode.next = node;
+			this.#head = curNode;
 		}
 
-		this.length += 1;
+		this.#length -= 1;
+	}
+	length() {
+		return this.#length;
+	}
+	isEmpty() {
+		return this.length() > 0 ? false : true;
 	}
 }
 
-const llist = new LinkList();
+const list = new LinkList();
 
-llist.AddNode(new Node('Dec'));
-llist.InsertNode(new Node('Jan'), 0);
-llist.AddNode(new Node('Feb'));
-llist.InsertNode(new Node('March'), 2);
-llist.InsertNode(new Node('April'), 4)
-llist.DisplayNodes();
-console.log(llist.GetLength());
+list.insert(2);
+list.insert(4);
+list.insert(5);
+
+console.log('\n length ', list.length());
+console.log(list.isEmpty());
+
+let iter1 = list.next();
+while (!iter1.done()) {
+	console.log(iter1.value());
+}
+list.del(2);
+console.log('\n length ', list.length());
+console.log(list.isEmpty());
+
+let iter2 = list.next();
+while (!iter2.done()) {
+	console.log(iter2.value());
+}
+list.del(4);
+console.log('\n length ', list.length());
+console.log(list.isEmpty());
+
+let iter3 = list.next();
+while (!iter3.done()) {
+	console.log(iter3.value());
+}
+
+list.del(5);
+console.log('\n length ', list.length());
+console.log(list.isEmpty());
+
+let iter4 = list.next();
+while (!iter4.done()) { //won't run.
+	console.log(iter4.value());
+}
