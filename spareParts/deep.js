55dbe5e374a59bbf0dced6882990f3240059b3eb
Kebalepile <44870957+Kebalepile@users.noreply.github.com>
2020-12-10 19:22:30 +0200

    Update deep.js

--- spareParts/deep.js
+++ spareParts/deep.js
@@ -4,42 +4,21 @@ let arr = [
 	[3, 5, 32, [45, 6, [45, 2, 1, 5], [6, 6, 7, [1, 7, 8, [5, [100, [20, [37, [23, [45, [204, [46046]]]]]]]], 9]]]],
 ];
 
-arr = arr.reduce((acc, cur) => {
-	if (Array.isArray(cur)) {
-		acc = [...acc, ...deepFlat(cur)];
-	} else {
-		acc.push(cur);
-	}
+const nestedAry = [1,2,[3,4,[5,6,7],8,9,10]];
 
-	return acc;
-}, []);
-
-/**
- * @param {Array} ary
- * @returns {Array}
- * @description
- * a Big-O(n) function that turns
- * a multi-demension Array into
- * a one demension Array.
- */
-function deepFlat(ary) {
-	if (ary.some((e) => Array.isArray(e))) {
-		return deepFlat(ary.flat()); //important piece !
-	}
-
-	return ary;
+function flatten(ary) {
+	const newAry = ary.reduce((acc, cur) => {
+		if (Array.isArray(cur)) {
+		    acc = acc.concat(flatten(cur));
+		
+		} else {
+			acc.push(cur);
+		}
+		return acc;
+	}, []);
+	return newAry;
 }
-console.log(arr);
-console.log(arr);
 
-// let acc = [];
 
-// for (let item of arr) {
-// 	if (Array.isArray(item)) {
-// 		acc.push(deepFlat(item));
-// 	} else {
-// 		acc.push(item);
-// 	}
-// 	acc = deepFlat(acc);
-// }
-// console.log(acc);
+console.log(flatten(nestedAry))
+console.log(flatten(arr)):
