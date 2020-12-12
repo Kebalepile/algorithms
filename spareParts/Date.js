54d7de8158a99a0d638abcdbf593fc4ae0acfb6b
Kebalepile <44870957+Kebalepile@users.noreply.github.com>
2020-12-12 08:36:40 +0200

    function for date and function for timestamp in js

--- spareParts/Date.js
+++ spareParts/Date.js
@@ -1,9 +1,21 @@
 export function date () {
-    let timeStamp = Date.now(),
+    const timeStamp = Date.now(),
     dateObject = new Date(timeStamp),
     date = dateObject.getDate(),
     month = dateObject.getMonth() + 1,
-    year = dateObject.getFullYear()
+    year = dateObject.getFullYear(),
+    hours = 12 - (12 - dateObject.getHours()),
+    minutes = dateObject.getMinutes()
 
     return `${year}/${month}/${date}`
+}
+
+
+export function currentTime(){
+     const timeStamp = Date.now(),
+     dateObject = new Date(timeStamp),
+     hours = 12 - (12 - dateObject.getHours()),
+     minutes = dateObject.getMinutes();
+    
+    return `${hours}:${minutes}`;
 }
